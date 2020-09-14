32dfa9404036bed9d496c76ba6889de48772fc49
Uetal <uetal@me.com>
2020-09-14 16:00:06 +0300

    14.09

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,24 @@
 package app.config;
 
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean
+    public Dog getDog(){
+        Dog dog = new Dog();
+        return dog;
+    }
+
+    @Bean
+    public Timer getTimer(){
+        Timer timer = new Timer();
+        return timer;
+    }
 }
--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -3,5 +3,5 @@ package app.model;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Dog extends Animal {
+public class Dog implements Animal {
 }
